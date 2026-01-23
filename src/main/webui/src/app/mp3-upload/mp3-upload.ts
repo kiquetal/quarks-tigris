0ac4ad4c27013ae33d6a1428efb6a4d44975817b
kiquetal <kiquetal@gmail.com>
2026-01-23 17:36:24 -0300

    add routing and authentication setup; implement passphrase validation and mp3 upload functionality

--- src/main/webui/src/app/mp3-upload/mp3-upload.ts
+++ src/main/webui/src/app/mp3-upload/mp3-upload.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mp3-upload',
-  imports: [HttpClientModule],
+  imports: [FormsModule],
   templateUrl: './mp3-upload.html',
   styleUrl: './mp3-upload.css',
 })
 export class Mp3Upload {
   selectedFile: File | null = null;
+  email = '';
 
   constructor(private http: HttpClient) {}
 
@@ -20,6 +22,7 @@ export class Mp3Upload {
     if (this.selectedFile) {
       const formData = new FormData();
       formData.append('file', this.selectedFile, this.selectedFile.name);
+      formData.append('email', this.email);
       this.http.post('/api/upload', formData).subscribe((res) => {
         console.log(res);
       });
--- src/main/webui/src/app/passphrase/passphrase.ts
+++ src/main/webui/src/app/passphrase/passphrase.ts
@@ -1,25 +1,27 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-passphrase',
-  imports: [HttpClientModule, FormsModule],
+  imports: [FormsModule],
   templateUrl: './passphrase.html',
   styleUrl: './passphrase.css',
 })
 export class Passphrase {
   passphrase = '';
-  @Output() passphraseValidated = new EventEmitter<void>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router, private authService: AuthService) {}
 
   validatePassphrase() {
     this.http
       .post('/api/validate-passphrase', { passphrase: this.passphrase })
       .subscribe((res: any) => {
         if (res.validated) {
-          this.passphraseValidated.emit();
+          this.authService.login();
+          this.router.navigate(['/upload']);
         } else {
           alert('Invalid passphrase');
         }
